c7377913361e39902fa7b9def179bb183b5f63cc
root <root@localhost>
2022-03-05 11:09:41 +0600

    auto publish 1646456980390

---
+++ db.php
@@ -0,0 +1,15 @@
+?php
+
+$server = "";
+$username = "epiz _25367495 ";
+$password = "em5xd2Q0ye KEJ1" ;
+$dbname = "epiz_ 25367495_packetcodeofficial";
+$conn
+= mysqli_connect ($server, $username, $password,$dbname );
+ 
+if(!$conn) {
+die("Connection Failed: ".mysqli_ connect_error ());
+
+}
+
+?>
